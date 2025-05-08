3f7848aa02268cd264d640365542f3ecf45c2326
sevinc995 <sevincseferova25@gmail.com>
2025-05-08 16:05:32 +0000

    js

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 "use strict";
 $(document).ready(function () {
 
+$('h1').click(function (e) {
+    e.preventDefault();
+    $('h1').html("HELLO");
+});
+
+$('.btn').click(function (e) {
+    e.preventDefault();
+    
+    let a = (a) => {
+        for (let i = a; i <=a; i++) {
+            $('.col-md-12').append(`<div class="minibox"> <h1>${i}</h1> </div>`);
+        }
+    }
+
+    a($('input').val())
+
+    
+});
+
 });
