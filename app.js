b05dddde223112ed5ac9be30015de027c9aef139
sevinc995 <sevincseferova25@gmail.com>
2025-05-15 09:26:04 +0000

    js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 $(document).ready(function () {
 
-
 $('.btn').click(function (e) {
     e.preventDefault();
     
@@ -10,11 +9,14 @@ $('.btn').click(function (e) {
             for (let i = a; i <=a; i++) {
                 $('.col-md-12').append(`<div class="minibox"> <h1>${i}</h1> </div>`);
             }
+
     }
 
     a($('input').val())
-
+    $("#inputme").val("")
     
 });
 
-});
+
+
+});  
