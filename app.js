c57ef66916cdd148444cd79148bb9bee1c959102
sevinc995 <sevincseferova25@gmail.com>
2025-05-22 13:09:51 +0000

    sass

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ $('.btn').click(function (e) {
     
     let a = (a) => {
         if (!$.trim($("#inputme").val()) == ' ')
+            
             for (let i = a; i <= a; i++) {
                 $('.col-md-12').append(`<div class="minibox"> <h1>${i}</h1> </div>`);
             }
@@ -16,6 +17,7 @@ $('.btn').click(function (e) {
     $("#inputme").val("")
     
 });
+
 
 
 
