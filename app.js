29ca3ae838f9a045c62094bf979763d090aa98cf
sevinc995 <sevincseferova25@gmail.com>
2025-06-20 20:12:06 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 "use strict";
 $(document).ready(function () {
 
-$('.btn').click(function (e) {
-    e.preventDefault();
+// $('.btn').click(function (e) {
+//     e.preventDefault();
     
-    let a = (a) => {
-        if (!isNaN ($("#inputme").val()) || isNaN ($("#inputme").val()) && !$.trim($("#inputme").val()) == ' ')
-            for (let i = a; i <= a; i++) {
-                $('.col-md-12').append(`<div class="minibox"> <h1>${i}</h1> </div>`);
+//     let a = (a) => {
+//         if (!isNaN ($("#inputme").val()) || isNaN ($("#inputme").val()) && !$.trim($("#inputme").val()) == ' ')
+//             for (let i = a; i <= a; i++) {
+//                 $('.col-md-12').append(`<div class="minibox"> <h1>${i}</h1> </div>`);
+//             }
+//     }
+
+//     a($('input').val())
+//     $("#inputme").val("")
+    
+// });
+
+    $('.btn').click(function (e) {
+        e.preventDefault();
+
+        if (!$.trim($('#inputme').val()) == '') {
+            let a = b => {
+                for (let i = 0; i < b; i++) {$('.minibox').append(`<h2>${b}</h2>`); }
             }
-    }
-
-    a($('input').val())
-    $("#inputme").val("")
-    
-});
-
+            // let v = f => !isNaN ($("#inputme").val()); {$('.minibox').append(`<h2>${b}</h2>`); }
+            // v($('#inputme').val());
+        }
+        // b($('input').val())
+        // f($('input').val())
+        $('#inputme').val('');
+       
+    });
 
 
 
