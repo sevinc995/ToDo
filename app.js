f3ae315925c53f70fbb2e0a3edad4d54b02d8d00
sevinc995 <sevincseferova25@gmail.com>
2025-07-03 11:11:17 +0000

    commited

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ $(document).ready(function () {
         if (!isNaN ($("input").val()) && !$.trim($('input').val()) == '') {
             let a = b => {
                 for (let i = 0; i < b; i++) {
-                    $('.col-md-12').append(`<div class="minibox"> <h2>${b}</h2> </div>`) 
+                    $('.col-md-12').append(`<div class="minibox"> <h2>${b}</h2> </div>`); 
                 }
             }
-            let v = f  => 
+            let v = f  => Number(f) ? a(f) : $('.col-md-12').append(`<div class="minibox"> <h2>${f}</h2> </div>`); 
             v($('input').val());
+            // a($('input').val());
         }
         $('#inputme').val('');
        
