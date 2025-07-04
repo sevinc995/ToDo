00c8a3c7144d3ccf0d67d2afc489790b9862df75
sevinc995 <sevincseferova25@gmail.com>
2025-07-04 12:27:45 +0000

    let changed

--- app.js
+++ app.js
@@ -22,20 +22,9 @@ $(document).ready(function () {
        
     })
 
-// $('.btn').click(function (e) {
-//     e.preventDefault();
-    
-//     let a = (a) => {
-//         if (!isNaN ($("#inputme").val()) || isNaN ($("#inputme").val()) && !$.trim($("#inputme").val()) == ' ')
-//             for (let i = a; i <= a; i++) {
-//                 $('.col-md-12').append(`<div class="minibox"> <h1>${i}</h1> </div>`);
-//             }
-//     }
 
-//     a($('input').val())
-//     $("#inputme").val("")
     
-// });
+
 
     
 
